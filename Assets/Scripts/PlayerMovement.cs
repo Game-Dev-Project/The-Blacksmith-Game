0dde38ec178bf814b056170b4ef0f47f6390e058
Alex <alexnova98@gmail.com>
2021-11-01 17:54:28 +0200

    Initial Comit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInputs();
+    }
+    void FixedUpdate()
+    {
+        move();
+    }
+    void processInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+
+    }
+    void move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
