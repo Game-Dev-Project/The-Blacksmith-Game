482357db71f1af4fc88c833b520f754edadec57d
Amit Karach <karach.amit30@gmail.com>
2022-01-02 20:39:03 +0200

    fixed trees and dialuge in lvl3

--- Assets/Scripts/NPC/WhatToPrass.cs
+++ Assets/Scripts/NPC/WhatToPrass.cs
@@ -17,7 +17,7 @@ public class WhatToPrass : MonoBehaviour
         //this.gameObject.GetComponent<NPC_dialogue>().enabled = true;
         if (other.gameObject.tag == "Player")
         {
-            //Debug.Log(this.name);
+            Debug.Log(this.name);
             KeyToPrass.SetActive(true);
             Vector3 Pos = Camera.main.WorldToScreenPoint(this.transform.position);
             Pos.y += 10;
